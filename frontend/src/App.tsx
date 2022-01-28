c06677cdcbcde8a6df32729a5a0a81a5328d7cd1
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-01-28 17:37:49 -0300

    feat: route configuration and folder structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import {ThemeProvider} from 'styled-components';
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import HomePage from './pages/Home';
+
+function App() {
+
+  const theme = {
+    primary: "#322153",
+    secondery: "#6c63ff",
+    background: "#F0F0F5",
+    text: "#6C6C80",
+    white: "#FFFFFF"
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <HomePage/>
+          </Route>
+            
+          <Route path="/new">Renderizando a new</Route>
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import {Title} from './styles'
+
+export default function Home(){
+  return (
+    <Title>Renderizando o componente home</Title>
+  )
+}
